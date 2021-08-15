a944e85754b95bbf1028d10e904617d0f7c411c3
Eduardo Luz <eduluz1976@gmail.com>
2021-08-15 12:06:49 -0700

    Adjust Artists component UI

--- src/screens/details/Artists.js
+++ src/screens/details/Artists.js
@@ -1,7 +1,7 @@
-import { GridList, ImageList, Typography } from '@material-ui/core';
+import { GridList, ImageList, TextField, Typography } from '@material-ui/core';
 import React, { Fragment } from 'react';
 import ArtistCard from '../../common/artist-card/ArtistCard';
-
+import './Artists.css';
 
 export default function Artists(props) {
 
@@ -9,9 +9,9 @@ export default function Artists(props) {
 
     return (
         <Fragment>
-        <Typography>
-            <b>Artists:</b>
-        </Typography>        
+            <Typography className="artist-title">
+             Artists:
+            </Typography>        
         <ImageList cols={2} rowHeight={180}>
         {movie.artists.map( item => {
             return (
