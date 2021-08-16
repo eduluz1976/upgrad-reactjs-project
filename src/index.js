475adb4634c20351464f351fa17e0da735a0347a
Eduardo Luz <eduluz1976@gmail.com>
2021-08-15 23:25:03 -0700

    Passing the baseUrl as props since the index
    Replace later by a config file contents.

--- src/index.js
+++ src/index.js
@@ -10,9 +10,12 @@ import { Provider } from 'react-redux';
 // import { ThemeProvider } from '@material-ui/core/styles';
 // import theme from './assets/theme';
 
+const config = {
+    baseUrl: 'http://abc/'
+};
 
 // ReactDOM.render(<ThemeProvider theme={theme}><Controller /></ThemeProvider>, document.getElementById('root'));
 ReactDOM.render(
-    <Provider store={store}><Controller /></Provider>
+    <Provider store={store}><Controller config={config} /></Provider>
     , document.getElementById('root'));
 registerServiceWorker();
--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -3,15 +3,21 @@ import Home from './home/Home';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import { Fragment } from 'react';
 import Details from './details/Details';
+import BookShow from './bookshow/BookShow';
 
 
-export default function Controller() {
+export default function Controller(props) {
+
+    const config = props.config;
+    console.log('Controller', props);
+
     return (
         <Fragment> 
             <Router>
                 <div>
-                    <Route exact path="/" render={ (props) => <Home {...props} /> }  />
-                    <Route  path="/movie/:id" render={ (props) => <Details {...props} /> }  />
+                    <Route exact path="/" render={ (props) => <Home {...props}  /> }  />
+                    <Route  path="/movie/:id" render={ (props) => <Details {...props}  baseUrl={config.baseUrl} /> }  />
+                    <Route  path="/book/:id" render={ (props) => <BookShow {...props}  baseUrl={config.baseUrl} /> }  />
                 </div>
             </Router>                                               
         </Fragment>
